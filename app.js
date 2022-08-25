6f8e5523d1574216f57ce95f134aa4f73b9a8cae
casanova <107594088+cfreemepq@users.noreply.github.com>
2022-08-25 19:49:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const { token } = require("./config.json");
 
 client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
-  client.user.setActivity("github.com/cfreemepq")
+  client.user.setActivity("usar")
 })
 
 client.on("message", message => {
   if (message.content === "/casanova") 
-    message.channel.send("https://github.com/cfreemepq \nhttps://twitter.com/cfreemepq")
+    message.channel.send("https://github.com/cfreemepq")
+})
+
+client.on("message", message => {
+  if (message.content === "/casanova") 
+    message.channel.send("https://www.roblox.com/users/3808139090/profile")
 })
 
 client.login(token);
